5550573f529c2eda9cdec73d573ff930e8be122d
phbomfim <paulobommfim@gmail.com>
2021-06-11 16:16:15 -0300

    Define a new_appointment method

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,4 +2,29 @@ class AppointmentsController < ApplicationController
     def index
         @appointments = Appointment.all
     end
+
+    def new
+        @appointment = Appointment.new
+    end
+
+    def create
+        @appointment = Appointment.new(appointment_params)
+        if @appointment.save
+            redirect_to appointments_path
+        else
+            render :new
+        end
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+        def appointment_params
+            params.require(:appointment).permit(:starts_at, :ends_at, :patient_id, :doctor_id)
+        end
 end
