c1b6ebe47a3020d7855bf47050ef2a3208060bc5
phbomfim <paulobommfim@gmail.com>
2021-06-11 15:57:30 -0300

    Define a new_doctor method

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,29 @@ class DoctorsController < ApplicationController
     def index
         @doctors = Doctor.all
     end
+
+    def new
+        @doctor = Doctor.new
+    end
+
+    def create
+        @doctor = Doctor.new(doctor_params)
+        if @doctor.save
+            redirect_to doctors_path
+        else
+            render :new
+        end
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+        def doctor_params
+            params.require(:doctor).permit(:name, :birth_date, :cpf)
+        end
 end
