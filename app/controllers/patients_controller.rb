6edd69874b12637373653cf24cbfafbb8f5d1c70
phbomfim <paulobommfim@gmail.com>
2021-06-11 15:43:25 -0300

    Create new_patient method

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,29 @@ class PatientsController < ApplicationController
     def index
         @patients = Patient.all
     end
+
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = Patient.new(patient_params)
+        if @patient.save
+            redirect_to patients_path
+        else
+            render :new
+        end
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+        def patient_params
+            params.require(:patient).permit(:name, :birth_date, :cpf)
+        end
 end
