4fffaf7cd7aae56d24e46fb2970eb251bcaaf384
phbomfim <paulobommfim@gmail.com>
2021-06-12 18:57:23 -0300

    Define patient validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,7 @@
 class Patient < ApplicationRecord
+
+    validates :name, :birth_date, :cpf, presence: true # All attributes are required.
+    validates :cpf, uniqueness: true # The system don't agree patients with same CPF
+
+    validates_cpf_format_of :cpf
 end
