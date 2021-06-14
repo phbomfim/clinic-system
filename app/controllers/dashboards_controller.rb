ea7324eb7df92719e3789fc7cf05bdcde974d3c6
phbomfim <paulobommfim@gmail.com>
2021-06-13 22:09:26 -0300

    Define dashboard filters

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,20 @@
 class DashboardsController < ApplicationController
-    def index
-        @dashboards
+
+    def show 
+        @doctors = Doctor.all
+        
+        if params[:filter].nil? || params[:filter] == "all"
+            @patients = Patient.all 
+            @appointments = Appointment.all
+        else
+            @appointments = Appointment.where(doctor_id: params[:filter])
+            @patients = Patient.all
+        end
     end
+
+    private 
+
+        def dashboards_params
+            params.require(:dashboard).permit(:filter, :doctor_id)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :patients
   resources :appointments
 
-  root to: 'dashboards#index'
+  post "show", to: "dashboards#show"
+
+  root to: 'dashboards#show'
 
 end
